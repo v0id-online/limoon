b16199fff0124570ff9497156eb9650cbe8b755d
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2025-08-25 22:23:15 -0400

    Fixed Qt progress dialog being stuck in indeterminate status.

--- src/textadept_qt.cpp
+++ src/textadept_qt.cpp
@@ -429,7 +429,7 @@ int save_dialog(DialogOptions opts, lua_State *L) { return open_save_dialog(&opt
 static void update(double percent, const char *text, void *dialog_) {
 	auto dialog = static_cast<QProgressDialog *>(dialog_);
 	if (percent >= 0)
-		dialog->setValue(percent);
+		dialog->setValue(percent), dialog->setMaximum(100); // also remove indeterminate status if set
 	else if (dialog->maximum() > 0)
 		dialog->setMaximum(0), dialog->show(); // switch to indeterminate and show immediately
 	if (text) dialog->setLabelText(text);
