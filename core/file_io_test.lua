c0e52b3b3e2bf7669810027306e86059c3bbe0fd
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2025-11-27 23:51:54 -0500

    Added missing unit test for `io.open_recent_file()`.

--- core/file_io_test.lua
+++ core/file_io_test.lua
@@ -548,6 +548,18 @@ test('buffer.close should use the global buffer', function()
 	test.assert_equal(buffer.filename, nil)
 end)
 
+test('io.open_recent_file should re-open a recent file', function()
+	local f<close> = test.tmpfile(true)
+	buffer.close()
+
+	local select_file = test.stub({1}, 1)
+	local _<close> = test.mock(ui.dialogs, 'list', select_file)
+
+	io.open_recent_file()
+
+	test.assert_equal(buffer.filename, f.filename)
+end)
+
 test('io.open_recent_file should allow clearing the list during prompt', function()
 	local _<close> = test.mock(io, 'recent_files', {})
 	local _<close> = test.tmpfile(true)
